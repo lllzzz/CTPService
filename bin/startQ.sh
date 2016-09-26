a7be58183a633fff535bdb34fbcef99e06f547bb
LaoZhu <shuiyun_5182604@163.com>
2016-09-26 15:52:31 +0800

    队列输入到tmp

--- bin/startQ.sh
+++ bin/startQ.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-nohup python ../src/market/queue.py ctp &
-nohup python ../src/trade/queue.py ctp &
+nohup python ../src/market/queue.py ctp > /tmp/mq.log &
+nohup python ../src/trade/queue.py ctp > /tmp/tq.log &
